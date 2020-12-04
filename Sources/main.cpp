dea318d5fbcfbadc404a2756b5b6902a2627e824
Tom <tom-vautray@hotmail.fr>
2020-12-04 18:01:28 +0100

    Added ideas of new menus

--- Sources/main.cpp
+++ Sources/main.cpp
@@ -3,26 +3,39 @@
 int main()
 {
 	//Creating menus
-	menu mainMenu, gamesMenu, actionsMenu, playerEditor, gameEditor;
+	menu mainMenu, gamesMenu, actionsMenu, playerEditor, gameEditor, gameActions, playerActions;
 
 	//	---	MENU OPTIONS	---	//
 	//Main menu options
 	mainMenu.addChoice("Games List");
 	mainMenu.addExit();
+
 	//Actions options
 	actionsMenu.addChoice("Play");
 	actionsMenu.addChoice("Edit Players");
 	actionsMenu.addChoice("Edit Game");
 	actionsMenu.addExit();
+
 	//Games Menu
 	/*for(game:games)
 		gamesMenu.addChoice(game.name)*/
 	gamesMenu.addExit();
+
 	//Player editor
 	/*for(player:players)
 		playerEditor.addChoice(player.name);*/
 	playerEditor.addChoice("New Player");
 	actionsMenu.addExit();
+
+	//Game actions
+	gameActions.addChoice("Continue");
+	gameActions.addChoice("Players Values");
+	gameActions.addExit();
+
+	//Player inGame actions
+	playerActions.addChoice("Capacities");
+	playerActions.addChoice("Move");
+	playerActions.addExit();
 
 	do
 	{
